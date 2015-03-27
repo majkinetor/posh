84be6b152041ee73bbd3a132e90bd812ec79083f
Miodrag Milic <miodrag.milic@gmail.com>
2015-03-27 13:34:53 +0100

    bugfix

--- HashTables/ConvertTo-HashTable.ps1
+++ HashTables/ConvertTo-HashTable.ps1
@@ -1,5 +1,4 @@
 #requires -version 2.0
-
 <#
 .Synopsis
     Convert an object into a hashtable.
@@ -7,8 +6,11 @@
     This command will take an object and create a hashtable based on its properties.
     You can have the hashtable exclude some properties as well as properties that
     have no value.
+.Notes
+    Author: Miodrag Milic <miodrag.milic@gmail.com>
+    Last Change: 27-Mar-2015.
 .Example
-    PS C:\> get-process -id $pid | select name,id,handles,workingset | ConvertTo-HashTable
+    get-process -id $pid | select name,id,handles,workingset | ConvertTo-HashTable
 
     Name                           Value
     ----                           -----
@@ -16,11 +18,6 @@
     Name                           powershell_ise
     Id                             3456
     Handles                        958
-.Notes
-    Version:  2.0
-    Updated:  January 17, 2013
-    Author :  Jeffery Hicks (http://jdhitsolutions.com/blog)
-    Modified: M Milic.
 .Link
     About_Hash_Tables
 .Inputs
@@ -29,40 +26,35 @@
     HashTable
 #>
 
-Function ConvertTo-HashTable {
+function ConvertTo-HashTable {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position=0,Mandatory=$True, HelpMessage="Please specify an object",ValueFromPipeline=$True)]
+        [ValidateNotNullorEmpty()]
+        # A PowerShell object to convert to a hashtable. If hashtable is passed, simply return it.
+        [object]$InputObject,
+        # Do not include object properties that have no value.
+        [switch]$NoEmpty,
+        # An array of property names to exclude from the hashtable.
+        [string[]]$Exclude
+    )
 
-[CmdletBinding()]
-Param(
-    [Parameter(Position=0,Mandatory=$True, HelpMessage="Please specify an object",ValueFromPipeline=$True)]
-    [ValidateNotNullorEmpty()]
-    # A PowerShell object to convert to a hashtable.
-    [object]$InputObject,
-    # Do not include object properties that have no value.
-    [switch]$NoEmpty,
-    # An array of property names to exclude from the hashtable.
-    [string[]]$Exclude
-)
+    Process {
 
-Process {
-    #get type using the [Type] class because deserialized objects won't have
-    #a GetType() method which is what we would normally use.
+        $TypeName = [system.type]::GetTypeArray($InputObject).name  # Deserialized objects won't have a GetType() method
+        if ($TypeName -eq [HashTable]) { return $InputObject }
 
-    $TypeName = [system.type]::GetTypeArray($InputObject).name
-    Write-Verbose "Converting an object of type $TypeName"
+        Write-Verbose "Converting an object of type $TypeName"
 
-    $names = $InputObject | gm -MemberType properties | select -expand name
-    $hash = @{}
-    $names | % {
-        if ($Exclude -notcontains $_) {
-            if ($NoEmpty -and -not ($inputobject.$_)) {
-                Write-Verbose "Skipping $_ as empty"
-            }
-            else {
-                Write-Verbose "Adding property $_"
-                $hash.Add($_,$inputobject.$_)
-            }
-        } else { Write-Verbose "Excluding $_" }
-    }  Write-Verbose "Writing the result to the pipeline"
-    $hash
- }
+        $names = $InputObject | gm -MemberType properties | select -expand name
+        $hash = @{}
+        $names | % {
+            if ($Exclude -contains $_) { return }
+            if ($NoEmpty -and -not ($inputobject.$_)) { Write-Verbose "Skipping empty $_ "; return }
+
+            Write-Verbose "Adding property $_"
+            $hash.Add($_,$inputobject.$_)
+        }
+        $hash
+    }
 }
