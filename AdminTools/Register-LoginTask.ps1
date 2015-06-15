2aeab2c7e440fe64d495b5d99260a1928f223335
Miodrag Milic <miodrag.milic@gmail.com>
2015-06-15 14:07:50 +0200

    bugfixes and docs - Register-LogiinTask

--- AdminTools/Register-LoginTask.ps1
+++ AdminTools/Register-LoginTask.ps1
@@ -1,4 +1,4 @@
-# Last Change: 2015-05-18.
+# Last Change: 15-Jun-2015.
 # Author: M. Milic <miodrag.milic@gmail.com>
 
 #requires -version 4.0
@@ -13,7 +13,29 @@
     because it has more options and behaves better with UAC. On the negative side, it
     requires elevated privileges.
 
-    The function creates the task inside the $Env:USERNAME\Startup path.
+    The function creates the task inside the $Env:USERDOMAIN\$Env:USERNAME\Startup path.
+
+.EXAMPLE
+    Register-LoginTask "$Env:PROGRAMFILES\Everything\Everything.exe" -Arguments '-startup' -RunElevated -Delay "00:00:30"
+
+    Starts everything.exe minimized in elevated mode with random 30 second delay.
+
+.EXAMPLE
+    '"Hello $($args[0])" > out.txt; ' > test.ps1
+    Register-LoginTask test.ps1 -Arguments 'Foo Bar' -Verbose
+
+    Register login script with arguments. Show Powershell arguments in verbose output.
+
+.EXAMPLE
+    Get-ScheduledTask -TaskPath \$Env:USERDOMAIN\$Env:USERNAME\*
+
+    List all registered tasks for the user.
+
+.EXAMPLE
+    Get-ScheduledTask -TaskPath \$Env:USERDOMAIN\$Env:USERNAME\* | select -First 1 | Unregister-ScheduledTask
+
+    Unregister first scheduled task for the user.
+
 #>
 function Register-LoginTask()
 {
@@ -23,8 +45,8 @@ function Register-LoginTask()
         [Parameter(Mandatory=$true, ValueFromPipeline=$True, Position=0)]
         [Alias("Path", "FullName")]
         [string]$Execute,
-        # Argument list to the program or script to execute
-        [string[]]$ArgumentList,
+        # Arguments to the program or script to execute
+        [string]$Arguments,
         # Maximum value of random delay, 0 by default
         [timespan] $Delay=(New-Timespan),
         # Execution limit, by default indefinite (more precise, 27.7 years)
@@ -34,10 +56,12 @@ function Register-LoginTask()
     )
 
     if (!(Test-Path $Execute)) { throw "Invalid path: $Execute" }
-    $script = (gi $Execute).Extension -eq '.ps1'
+    $isScript = (gi $Execute).Extension -eq '.ps1'
 
     $user = "$env:USERDOMAIN\$env:USERNAME"
-    $a = New-ScheduledTaskAction -Execute $Execute -Argument "$ArgumentList"
+    $params = @{ Execute = $Execute }
+    if (![string]::IsNullOrWhiteSpace($Arguments)) { $params.Argument = $Arguments }
+    $a = New-ScheduledTaskAction @params
     $t = New-ScheduledTaskTrigger -AtLogon -User $user -RandomDelay $Delay
     $s = New-ScheduledTaskSettingsSet -ExecutionTimeLimit $Limit  -DontStopIfGoingOnBatteries -AllowStartIfOnBatteries -Compatibility Win8 -StartWhenAvailable
 
@@ -50,9 +74,13 @@ function Register-LoginTask()
         Taskname = (Split-Path -Leaf $Execute)
     }
     if ($RunElevated) {$params.RunLevel="Highest"}
-    if ($script) {
-        $params.TaskName = "Powershell Login Script"
-        $params.Action   = New-ScheduledTaskAction -Execute "$PSHome\powershell.exe" -Argument "-WindowStyle Hidden -NoProfile -NoLogo -NonInteractive -ExecutionPolicy Bypass -File $ScriptPath $ArgumentList"
+    if ($isScript) {
+        $params.TaskName = "PS - $(Split-Path -Leaf $Execute)"
+        $scriptPath = Resolve-Path $Execute
+        $sa = "-NoProfile -NoLogo -WindowStyle Hidden -NonInteractive -ExecutionPolicy Bypass -Command `"cd `$HOME; . '$scriptPath' $Arguments`""
+        Write-Verbose "Registering powershell logon script. Powershell arguments:`n$sa"
+        $params.Action = New-ScheduledTaskAction -Execute "$PSHome\powershell.exe" -Argument $sa
     }
+
     Register-ScheduledTask @params
 }
