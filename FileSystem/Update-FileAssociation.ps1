b926075a7c08bba639c0a7d44e6066b364860521
Miodrag Milic <miodrag.milic@gmail.com>
2015-06-16 01:01:41 +0200

    dot on extension; docs

--- FileSystem/Update-FileAssociation.ps1
+++ FileSystem/Update-FileAssociation.ps1
@@ -1,5 +1,5 @@
 # Author: Miodrag Milic <miodrag.milic@gmail.com>
-# Last Change: 15-Jun-2015.
+# Last Change: 16-Jun-2015.
 
 #requires -version 2.0
 
@@ -16,20 +16,20 @@
 .EXAMPLE
     assoc temp tempFile "`"$((gcm gvim).Definition)`" --remote-tab-silent %1" | fl
 
-    Associate .temp extension to gvim and if possible start it in already open vim window.
+    Associate .temp extension to gvim and try to open files in a new tab of existing instance.
 
 .EXAMPLE
     assoc html,txt | select -Expand Executable
 
     Get the list of associated programs for given extensions.
 
-.NOTE
+.NOTES
     Setting file association requires eleveated privileges.
 #>
 function Update-FileAssociation
 {
     param(
-        # Extension (without the dot) for which to get/set file type and associated command.
+        # Extension (with or without the dot) for which to get/set file type and associated command.
         [Parameter(ValueFromPipeline=$true, Mandatory=$true, Position=0)]
         [string[]]$Extension,
 
@@ -48,17 +48,20 @@ function Update-FileAssociation
     $result =  "" | select Extension, FileType, Command, Executable
     $results = @()
 
+    function parse-command {$args[0]}
     $set  = "FileType","Command" | ? {$PSBoundParameters.Keys -contains $_ }
     foreach ($ext in $Extension)
     {
         $r = $result.PSObject.Copy()
+        if (!$ext.StartsWith('.')) {$ext = ".$ext"}
+
         $r.Extension = $ext
 
         Write-Verbose "Reading registry for extension: $ext"
-        $ftype = gp "HKCR:\.$ext" -ea ignore
+        $ftype = gp "HKCR:\$ext" -ea ignore
         if ($ftype) {
             $r.FileType = $ftype.'(default)'
-            if ($r.FileType) { 
+            if ($r.FileType) {
                 Write-Verbose "Reading registry for file type: $($r.FileType)"
                 $assoc = gp "HKCR:\$($r.FileType)\shell\open\command" -ea ignore
                 if ($assoc){ $r.Command = $assoc.'(default)' }
@@ -68,7 +71,7 @@ function Update-FileAssociation
         if ($set) {
             if ($FileType -ne $null) {
                 Write-Verbose "Setting file type for file extension: $ext"
-                "HKCR:\.$ext" | % {
+                "HKCR:\$ext" | % {
                     mkdir $_ -ea ignore | Out-Null
                     sp $_ '(default)' ($r.FileType = $FileType)
                 }
@@ -82,7 +85,6 @@ function Update-FileAssociation
             }
         }
 
-        function parse-command {$args[0]}
         $r.Executable =  iex "parse-command $($r.Command)"
         $results += $r
     }
@@ -91,12 +93,5 @@ function Update-FileAssociation
 
 sal assoc Update-FileAssociation
 
-assoc temp tempFile "`"$((gcm gvim).Definition)`" --remote-tab-silent %1" -Verbose | fl
-
+# Relevant docs
 #https://msdn.microsoft.com/en-us/library/windows/desktop/ms724498(v=vs.85).aspx
-# ima smisla da bude i FileType nezavisno od Ext... recimo Drive i Directory
-# videti kako se hendluje NoOpen
-#https://msdn.microsoft.com/en-us/library/windows/desktop/ms724498(v=vs.85).aspx
-#The NoOpen designation is for files that you don't want users to open. When the user double-clicks a file marked as NoOpen, the operating system will automatically provide a message informing the user that the file should not be opened. Note that if an action is later associated with a NoOpen file type, the NoOpen designation will be ignored.
-
-#Pojavljuje se poruka 'you are attemtping to open file used by the system...'
