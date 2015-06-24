982224e842a0e2af04bf6206eed017eee21b609e
Miodrag Milic <miodrag.milic@gmail.com>
2015-06-24 08:29:31 +0200

    enumerate all interfaces

--- MMNetwork/Get-WirelessNetworks.ps1
+++ MMNetwork/Get-WirelessNetworks.ps1
@@ -1,31 +1,34 @@
 ﻿# Author: Miodrag Milic <miodrag.milic@gmail.com>
-# Last Change: 23-Jun-2015.
+# Last Change: 24-Jun-2015.
 
 #requires -version 3
 
 <#
 .SYNOPSIS
-    List given interface wireless networks
+    List wireless networks
 
 .DESCRIPTION
     Uses netsh wlan to get wireless network information in Powershell friendly way.
-    By default uses Wi-Fi interface name.
 #>
 function Get-WirelessNetworks([string]$Interface='Wi-Fi') {
 
     if ((gwmi win32_operatingsystem).Version.Split(".")[0] -lt 6) { throw "Requires Windows Vista or higher." }
     if ((gsv "wlansvc").Status -ne "Running" ) { throw "Wlan service is not running." }
 
-    $props = @( 'SSID', 'Authentication', 'Encryption', 'Signal', 'Radiotype', 'Channel', 'BSSID' )
+    $ifaces = netsh wlan show interfaces | sls '^\s*Name\s*:\s*(.+)\s*' | % { $_.matches[0].Groups[1].Value }
 
+    $props = @( 'SSID', 'Authentication', 'Encryption', 'Signal', 'Radiotype', 'Channel', 'BSSID', 'Interface' )
     $nt = "" | select -Property $props
     $n  = $nt.PSObject.Copy()
     $results = @()
-    netsh wlan show network mode=bssid interface=$Interface | select -Skip 4 | % {
-        if (!$_) { $results += $n; $n = $nt.PSObject.Copy(); return }
-        $a =  $_ -split ' : '
-        $p = $a[0].Trim() -replace '[ \d]*'; $v = $a[1].Trim()
-        if ($props -contains $p) { $n.$p = $v }
+    $ifaces | % {
+        $iface = $_
+        netsh wlan show network mode=bssid interface="$iface" | select -Skip 4 | % {
+            if (!$_) { $n.Interface = $iface; $results += $n; $n = $nt.PSObject.Copy(); return }
+            $a =  $_ -split ' : '
+            $p = $a[0].Trim() -replace '[ \d]*'; $v = $a[1].Trim()
+            if ($props -contains $p) { $n.$p = $v }
+        }
     }
     $results
 }
