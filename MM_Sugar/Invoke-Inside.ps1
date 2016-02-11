872c784a5f3e81bb1699cf28afbc6983c569af6f
Miodrag Milic <miodrag.milic@gmail.com>
2016-02-11 21:44:28 +0100

    docs

--- MM_Sugar/Invoke-Inside.ps1
+++ MM_Sugar/Invoke-Inside.ps1
@@ -1,6 +1,15 @@
-
-<# .SYNOPSIS
+<#
+.SYNOPSIS
     Execute script in given directory or given files directory
+.EXAMPLE
+    PS> in c:\windows ls
+
+    Executes the ls command in the given directory without changing the current directory.
+
+.EXAMPLE
+    PS> in c:\windows\notepad.exe ls
+
+    If the directory is a file, the script will execute within its parent.
 #>
 function Invoke-Inside ($dir, $script) {
     if (Test-Path $dir -PathType Leaf) { $dir = Split-Path $dir -Parent }
