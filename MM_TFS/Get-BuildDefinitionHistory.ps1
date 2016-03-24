162c8489c9d42974e2371c51e3facbb448ef91ac
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-24 18:33:02 +0100

    id_to_name

--- MM_TFS/Get-BuildDefinitionHistory.ps1
+++ MM_TFS/Get-BuildDefinitionHistory.ps1
@@ -1,6 +1,7 @@
 function Get-BuildDefinitionHistory($Id, [switch]$Raw) {
     if ($Id -eq $null) { throw "Resource with that name doesn't exist" }
     if ($Id.GetType() -eq [string]) { $Id = Get-BuildDefinitions | ? name -eq $Id | select -Expand id }
+
     $uri = "$proj_uri/_apis/build/definitions/$($Id)/revisions?api-version=" + $tfs.api_version
     $r = Invoke-RestMethod -Uri $uri -Method Get -Credential $tfs.credential
     if ($Raw) { return $r }
--- MM_TFS/Get-Project.ps1
+++ MM_TFS/Get-Project.ps1
@@ -2,6 +2,7 @@
 function Get-Project($Id) {
 
     if ($Id.GetType() -eq [string]) { $Id = Get-Projects | ? name -eq $Id | select -Expand id }
+    if ($Id -eq $null) { throw "Can't find project with that name or id" }
 
     $uri = "$collection_uri/_apis/projects/$($Id)?includeCapabilities=true&api-version=" + $tfs.api_version
     $r = Invoke-RestMethod -Uri $uri -Method Get -Credential $tfs.credential
