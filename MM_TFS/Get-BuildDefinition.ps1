6809da28929a179f7de782913a655b490712c038
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-24 18:10:57 +0100

    fix

--- MM_TFS/Get-BuildDefinition.ps1
+++ MM_TFS/Get-BuildDefinition.ps1
@@ -1,7 +1,7 @@
 function Get-BuildDefinition($Id, [switch]$Raw, [switch]$Export) {
 
-    if ($Id -eq $null) { throw "Resource with that name doesn't exist" }
     if ($Id.GetType() -eq [string]) { $Id = Get-BuildDefinitions | ? name -eq $Id | select -Expand id }
+    if ($Id -eq $null) { throw "Resource with that name doesn't exist" }
 
     $uri = "$proj_uri/_apis/build/definitions/$($Id)?api-version=" + $tfs.api_version
     $r = Invoke-RestMethod -Uri $uri -Method Get -Credential $tfs.credential
