d5e0b2f2c1c1a35fb9fe8194be7561c1d62bc00b
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-24 18:38:26 +0100

    value

--- MM_TFS/Get-Projects.ps1
+++ MM_TFS/Get-Projects.ps1
@@ -2,7 +2,7 @@
 function Get-Projects([switch]$Raw ) {
     $uri = "$collection_uri/_apis/projects?api-version=" + $tfs.api_version
     $r = Invoke-RestMethod -Uri $uri -Method Get -Credential $tfs.credential
-    if ($Raw) { return $r }
+    if ($Raw) { return $r.value }
 
     $res = $r.value | select name, description, revision, id
     $res | ft -auto
