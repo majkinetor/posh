ec8c744c90581d989d8470a84a28277301e11248
Miodrag Milic <miodrag.milic@gmail.com>
2016-03-25 13:36:01 +0100

    bugfixes

--- MM_TFS/Get-BuildLogs.ps1
+++ MM_TFS/Get-BuildLogs.ps1
@@ -6,8 +6,14 @@ function Get-BuildLogs($Id=$null) {
     $r = Invoke-RestMethod -Uri $uri -Method Get -Credential $tfs.credential
 
     $lines = @()
+    $root_server_name = $tfs.root_url -split '/' | select -Index 2
     foreach ( $url in $r.value.url ) {
-        $l = Invoke-RestMethod -Uri $url -Method Get -Credential $tfs.credential
+        #TFS might return non FQDM so its best to replace its server name with the one user specified
+        $new_url = $url -split '/'
+        $new_url[2] = $root_server_name
+        $new_url = $new_url -join '/'
+
+        $l = Invoke-RestMethod -Uri $new_url -Method Get -Credential $tfs.credential
         $lines += $l.value -replace '\..+?Z'
         $lines += "="*150
     }
--- MM_TFS/Get-Project.ps1
+++ MM_TFS/Get-Project.ps1
@@ -1,7 +1,7 @@
 
 function Get-Project($Id) {
 
-    if ($Id.GetType() -eq [string]) { $Id = Get-Projects | ? name -eq $Id | select -Expand id }
+    if ($Id.length -ne 36) { $Id = Get-Projects -Raw | ? name -eq $Id | select -Expand id }
     if ($Id -eq $null) { throw "Can't find project with that name or id" }
 
     $uri = "$collection_uri/_apis/projects/$($Id)?includeCapabilities=true&api-version=" + $tfs.api_version
