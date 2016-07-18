06b5685df4a2b423499feced9d80469d93bb08c6
Miodrag Milic <miodrag.milic@gmail.com>
2016-07-18 11:57:59 +0200

    works on file paths too

--- MM_Sugar/ClipboardDir.ps1
+++ MM_Sugar/ClipboardDir.ps1
@@ -1,7 +1,13 @@
 #requires -version 5
 
 function Set-ClipboardDir() { "$pwd" | Set-Clipboard }
-function Get-ClipboardDir() { Get-Clipboard | cd }
+function Get-ClipboardDir() {
+    $item = Get-Clipboard
+    if (!(Test-Path $item)) { throw "Clipboard item is not a valid path" }
+
+    if (!(gi $item).PSIsContainer) { $item = Split-Path $item }
+    pushd $item
+}
 
 sal cpushd Set-ClipboardDir
-sal cpopd Get-ClipboardDir
+sal cpopd  Get-ClipboardDir
