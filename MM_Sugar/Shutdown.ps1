7c3806e8ea978a8b69f9e7fc899853885a02e68f
Miodrag Milic <miodrag.milic@gmail.com>
2017-02-15 13:32:20 +0100

    shutdown fixed; _Aliases removed (now in .profiled);

--- MM_Sugar/Shutdown.ps1
+++ MM_Sugar/Shutdown.ps1
@@ -1,12 +1,12 @@
-function Invoke-PSShutdown { shutdown /t 0 }
-function Invoke-PSReboot { shutdown /t 0 /r }
-function Invoke-PSHibernate {
+function Invoke-Shutdown { shutdown /t 0 /s}
+function Invoke-Reboot { shutdown /t 0 /r }
+function Invoke-Hibernate {
     $enabled = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\Power -name HibernateEnabled | select -Expand HibernateEnabled
     if (!$enabled) { return 'Hibernation is not enabled on this system. To enable it, run elevated: powercfg.exe /H ON' }
 
     shutdown /h
 }
 
-sal halt Invoke-PSShutdown
-sal hiber Invoke-PSHibernate
-sal reboot Invoke-PSReboot
+sal halt   Invoke-Shutdown
+sal hiber  Invoke-Hibernate
+sal reboot Invoke-Reboot
--- MM_Sugar/_Aliases.ps1
+++
@@ -1,8 +0,0 @@
-sal remove  Remove-Module
-sal import  Import-Module
-sal new     New-Object
-
-#Remove problematic default aliases
-rm -ea 0 `
-    alias:curl,
-    alias:wget
