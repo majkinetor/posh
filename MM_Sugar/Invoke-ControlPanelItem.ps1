0cf6e0ded55d8ab0d0f8042b4ea7830c40b67189
Miodrag Milic <miodrag.milic@gmail.com>
2017-04-17 22:28:36 +0100

    control sugar function

---
+++ MM_Sugar/Invoke-ControlPanelItem.ps1
@@ -0,0 +1,12 @@
+#require -version 3
+
+<#
+.SYNOPSIS 
+    Invoke control papnel item by filtering out the item list with fuzzy search
+#>
+function Invoke-ControlPanelItem() { 
+    if (!(gcm fzf -ea 0)) { Write-Host "To use this function, install fzf first: cinst fzf"; return }
+    Get-ControlPanelItem  | % name | sort | fzf | Show-ControlPanelItem 
+}
+
+sal control Invoke-ControlPanelItem
