cfafdbb23263a323baa915be7e2d5bb376373185
Miodrag Milic <miodrag.milic@gmail.com>
2017-06-20 12:59:25 +0200

    alias stasks and path changed

--- MM_Admin/Show-UserTasks.ps1
+++ MM_Admin/Show-UserTasks.ps1
@@ -1,6 +1,5 @@
 <# .SYNOPSIS
     Returns all scheduled tasks under the username Path in Task Scheduler
 #>
-
-function Show-UserTasks() { Get-ScheduledTask -TaskPath *$Env:USERNAME* }
-sal tasks Show-UserTasks
+function Show-UserTasks() { Get-ScheduledTask -TaskPath "\$Env:USERDOMAIN\$Env:USERNAME\*" | sort State}
+sal stasks Show-UserTasks
