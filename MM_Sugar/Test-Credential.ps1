6245378e3de7b52e6aa186f96cd645bbdfbbd2c4
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-13 15:38:26 +0200

    test-credential

---
+++ MM_Sugar/Test-Credential.ps1
@@ -0,0 +1,27 @@
+<#
+.SYNOPSIS
+    Takes a PSCredential object and validates it against the domain (or local machine, or ADAM instance).
+
+.LINK
+    https://serverfault.com/a/286003/23290
+#>
+function Test-Credential {
+    param(
+        # A PScredential object with the username/password to test. Typically this is generated using the Get-Credential cmdlet.
+        [parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PSCredential] $Credential,
+
+        # An optional parameter specifying what type of credential this is. Possible values are 'Domain','Machine',and 'ApplicationDirectory.' The default is 'Domain'.
+        [validateset('Domain','Machine','ApplicationDirectory')]
+        [string] $Context = 'Domain'
+    )
+
+    begin {
+        Add-Type -AssemblyName System.DirectoryServices.AccountManagement
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::$Context) 
+    }
+
+    process {
+        $DS.ValidateCredentials( $Credential.UserName, $Credential.GetNetworkCredential().Password )
+    }
+}
