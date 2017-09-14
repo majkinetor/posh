0371a615e6c149fb34b5e883d7b8d064a4ac82bd
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-14 11:06:52 +0200

    merge-hashtables

---
+++ MM_HashTables/Merge-Hashtables.ps1
@@ -0,0 +1,73 @@
+#requires -Version 2.0
+<#
+.SYNOPSIS
+    Create new HashTable from two HashTables where the second given
+    HashTable will override.
+
+.DESCRIPTION
+    In case of duplicate keys the second HashTable's key values "win". 
+    Nested HashTables are supported.
+
+.EXAMPLE
+    $configData = Merge-Hashtables -First $defaultData -Second $overrideData
+
+.INPUTS
+    None
+
+.OUTPUTS
+    System.Collections.Hashtable
+#>
+function Merge-Hashtables
+{
+    [CmdletBinding()]
+    param (
+        # Base HashTable
+        [Parameter(Mandatory=$true)] [Hashtable] $First,
+
+        # Overriding HashTable
+        [Parameter(Mandatory=$true)] [Hashtable] $Second
+    )
+
+    function Set-Keys ($First, $Second)
+    {
+        @($First.Keys) | ? { $Second.ContainsKey($_) } | % {
+            if (($First.$_ -is [Hashtable]) -and ($Second.$_ -is [Hashtable])) {
+                Set-Keys -First $First.$_ -Second $Second.$_
+            }
+            else {
+                $First.Remove($_)
+                $First.Add($_, $Second.$_)
+            }
+        }
+    }
+
+    function Add-Keys ($First, $Second)
+    {
+        @($Second.Keys) | % {
+            if ($First.ContainsKey($_)) {
+                if (($Second.$_ -is [Hashtable]) -and ($First.$_ -is [Hashtable])) {
+                    Add-Keys -First $First.$_ -Second $Second.$_
+                }
+            }
+            else {
+                $First.Add($_, $Second.$_)
+            }
+        }
+    }
+
+    function clone( $DeepCopyObject )  {
+        $memStream = new-object IO.MemoryStream
+        $formatter = new-object Runtime.Serialization.Formatters.Binary.BinaryFormatter
+        $formatter.Serialize($memStream,$DeepCopyObject)
+        $memStream.Position=0
+        $formatter.Deserialize($memStream)
+    }
+    
+    $firstClone  = clone $First
+    $secondClone = clone $Second
+
+    Set-Keys -First $firstClone -Second $secondClone
+    Add-Keys -First $firstClone -Second $secondClone
+
+    $firstClone
+}
