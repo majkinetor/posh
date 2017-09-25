b054f7d495862551bab9e29a247efb89e8b47e2d
Miodrag Milic <miodrag.milic@gmail.com>
2017-09-25 13:34:00 +0200

    docs

--- MM_Sugar/Get-ISO8601Time.ps1
+++ MM_Sugar/Get-ISO8601Time.ps1
@@ -1,5 +1,6 @@
 <# .SYNOPSIS
-    Get current time in format ISO8601 yyyy-MM-ddTHH-mm-ss
+    Get current time in format ISO8601 yyyy-MM-ddTHH-mm-ss with : of time replaced with -
+    so it can be used with Windows filenames
 #>
 function Get-ISO8601Time([switch]$fs) {[DateTime]::Now.ToString("s").Replace(':','-')}
 sal now Get-ISO8601Time
