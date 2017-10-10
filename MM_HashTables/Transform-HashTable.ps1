f4f615b622e05dcad7612a46d8f7ea7264f65bc2
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-10 10:41:33 +0200

    Transform-HashTable improved

--- MM_HashTables/Transform-HashTable.ps1
+++ MM_HashTables/Transform-HashTable.ps1
@@ -1,32 +1,69 @@
-# Transform hashtable to another hashtable
-#   Action params: key, value, parent (if value is array, action will be called with $null key)
-#   Action output: new key, new_value ($null as a key deletes key on the output in both hash and array)
-function Transform-HashTable ([HashTable]$Hash, [ScriptBlock] $Action, [string]$Parent ) {
-    $res = @{}
+<#
+.SYNOPSIS
+    Transform given hashtable via deep iterator to new hashtable
+
+.DESCRIPTION
+    Provide a scriptblock that processes each element of an existing hashtable. Change, add or remove elements by 
+returning array of key/value pairs.
+
+.PARAMETER Hash
+    ([ordered]) Hashtable to be transformed
+
+.PARAMETER Action
+    ScriptBlock to call on each element. Accepts arguments $key, $value, $parentpath. Parent path is dot joined list of all parents.
+    Returns array (key, val, key1, val1, .... keyN, valN).
+    First 2 will be used instead of the original key/val pairs. Pass $null as key to delete the key/value pair in the resulting table.
+    Other keys (1..N) will be addeded to resulting table.
+    
+    If value is an [Array] and $EnumerateArrays switch is present, the Action will get called with the $key parameter set to $null and the $value parameter set to array value.
+    Return $false to remove array item from resulting array or ($true, $item1 ... $itemN) to replace the current $value with $items[1..N].
+
+.EXAMPLE
+    Transform-HashTable @{x=1; y=2; z=@{p=3}} { param( $key, $value ) $key.ToUpper(), $value  }
+
+    Transform keys to uppercase
+
+.EXAMPLE
+    Transform-HashTable ([ordered]@{x=1; y=2; z=1,2,3,4}) { param( $key, $value ) 
+        if (!$key) { return $true, $value, ($value*10) } 
+        else { 
+            if ($value -isnot [Array]) { $key, $value, "$key*10", ($value*10) }  else { $key, $value }
+        }
+    }
+
+    Transform hashtable to multiply each key with 10 and add it as a new hashtable element.
+    If array is encountered, each element is multiplied with 10 and added to the array besides original elements.
+
+#>
+function Transform-HashTable ($Hash, [ScriptBlock] $Action, [switch] $EnumerateArrays, [Parameter(DontShow = $true)] [string] $Parent ) {
+    function is_hashtable { ($args[0] -is [HashTable]) -or ($args[0] -is [System.Collections.Specialized.OrderedDictionary]) }
+
+    $res = if ($Hash -is [HashTable]) { @{} } else { [ordered]@{} }
 
     $Hash.Keys | % {
         $key = $_
         $val = $Hash.$key
 
         if ($val -ne $null) {
-            if ($val.GetType() -eq [HashTable]) {
-                $h = transform_hash $val $Action "$($Parent).$($key)"
-                $res.$key = $h
-                return
+            if (is_hashtable $val) {
+                $val = Transform-HashTable $val $Action "$($Parent).$($key)"
             }
-            if ($val.GetType() -eq [Object[]]) {
-                $a =@()
-                $val | % {
-                    $k,$v = & $Action $null $_ "$($Parent).$($key)"
-                    if ($k){ $a+=$v }
+
+            if (($val -is [Array]) -and $EnumerateArrays) {
+                $a=@()
+                foreach ($item in $val) {
+                    $b = & $Action $null $item "$($Parent).$($key)"
+                    if ( !$b[0] ) { continue }
+                    $a += $b[1..($b.Count)]                    
                 }
-                $res.$key = $a
-                return
+                $val = $a
             }
         }
-        $k,$v = & $Action $key $val $parent
-        if ($k){ $res.$k  = $v }
+        $a = & $Action $key $val $parent
+        for ($i=0; $i -lt $a.Count; $i+=2) {
+            $k = $a[$i]; $v = $a[$i+1]
+            if ($k) { $res.$k  = $v }
+        }
     }
     $res
 }
-
