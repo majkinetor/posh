4e9bdd896620527ee7ad2800589f9602ff93ea53
Miodrag Milic <miodrag.milic@gmail.com>
2017-10-12 11:04:52 +0200

    fix

--- MM_Network/Stop-ProcessByPort.ps1
+++ MM_Network/Stop-ProcessByPort.ps1
@@ -10,8 +10,10 @@ function Stop-ProcessByPort( [ValidateNotNullOrEmpty()] [int] $Port ) {
     $p_line  = $netstat | ? { $p = (-split $_ | select -Index 1) -split ':' | select -Last 1; $p -eq $Port } | select -First 1
     if (!$p_line) { Write-Host "No process found using port" $Port; return }    
     $p_id = $p_line -split '\s+' | select -Last 1
+    if ($p_id) { throw "Can't parse process id" }
     
     $proc = Get-WmiObject win32_process -Filter "ProcessId = $p_id"
+    if (!$proc) { Write-Host "Process with pid $p_id using port $Port is no longer running" }
     $proc.Terminate() | Out-Null
 
     #$p_name = ps -Id $p_id -ea 0 | kill -Force -PassThru | % ProcessName
