6ad2bbe7040603cb8a4deaeae50bb79167b7c9ce
Miodrag Milic <miodrag.milic@gmail.com>
2019-11-14 15:30:29 +0100

    added better note

--- MM_Sugar/Shutdown.ps1
+++ MM_Sugar/Shutdown.ps1
@@ -2,7 +2,10 @@ function Invoke-Shutdown { shutdown /t 0 /s}
 function Invoke-Reboot { shutdown /t 0 /r }
 function Invoke-Hibernate {
     $enabled = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\Power -name HibernateEnabled | select -Expand HibernateEnabled
-    if (!$enabled) { return 'Hibernation is not enabled on this system. To enable it, run elevated: powercfg.exe /H ON' }
+    if (!$enabled) { return 'Hibernation is not enabled on this system. 
+     To enable it, run elevated: powercfg.exe /H ON
+     Alternativelly, go to `Control Panel\Hardware and Sound\Power Options\System Settings` to enable it.'
+    }
 
     shutdown /h
 }
