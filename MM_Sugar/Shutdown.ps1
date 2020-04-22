7cd9583f3e35678ae6947525c3035a3053ac99c6
Miodrag Milic <miodrag.milic@gmail.com>
2020-04-22 16:11:13 +0200

    wait-action; shutdown fix

--- MM_Sugar/Shutdown.ps1
+++ MM_Sugar/Shutdown.ps1
@@ -1,6 +1,8 @@
-function Invoke-Shutdown { shutdown /t 0 /s}
-function Invoke-Reboot { shutdown /t 0 /r }
-function Invoke-Hibernate {
+function Invoke-Shutdown { shutdown.exe /t 0 /s}
+function Invoke-Reboot { shutdown.exe /t 0 /r }
+function Invoke-Logout { logoff.exe }
+
+function Invoke-Hibernation {
     $enabled = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Control\Power -name HibernateEnabled -ea 0 | select -Expand HibernateEnabled
     if (!$enabled) {  
         Write-Warning 'Hibernation doesn''t seem to be enabled on this system.'
@@ -8,9 +10,13 @@ function Invoke-Hibernate {
         Write-Warning 'Alternativelly, go to `Control Panel\Hardware and Sound\Power Options\System Settings` to enable it.'
     }
 
-    shutdown /h
+    shutdown.exe /h
 }
 
-sal halt   Invoke-Shutdown
-sal hiber  Invoke-Hibernate
-sal reboot Invoke-Reboot
+
+sal halt      Invoke-Shutdown
+sal suspend   Invoke-Hibernation
+sal hibernate Invoke-Hibernation
+sal reboot    Invoke-Reboot
+sal logout    Invoke-Logout
+sal logoff    Invoke-Logout
---
+++ MM_Sugar/Wait-Action.ps1
@@ -0,0 +1,25 @@
+function Wait-Action ([ScriptBlock]$Action, [int]$Timeout=20) {
+    Write-Host "Waiting for action to succeed up to $Timeout seconds"
+
+    $start = Get-Date
+    while ($true) {
+        $elapsed = ((Get-Date) - $start).TotalSeconds
+        if ($elapsed -ge $Timeout) {break}
+
+        $j = Start-Job $Action
+        $maxWait = [int]($Timeout-$elapsed)
+        if ($maxWait -lt 1) { $maxWait = 1 }
+        Wait-Job $j -Timeout $maxWait | Out-Null
+
+        if ($j.State -eq 'Running') { $err = 'still running'; break }
+        if ($j0 = $j.ChildJobs[0]) {
+            if ($err = $j0.Error) { continue }
+            if ($err = $J0.JobStateInfo.Reason) {continue}
+        }
+        try { Receive-Job $j -ErrorAction STOP | Out-Null; } catch { $err = "$_"; continue }
+
+        Write-Host "Action succeded"; return
+    }
+
+    throw "Action timedout. Last error: $err"  
+}
