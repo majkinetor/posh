431bef5d273f463ef0a94d23d764cd80a500a3d9
Miodrag Milic <miodrag.milic@gmail.com>
2023-09-30 10:48:05 +0200

    Set-UncHostnameAlias improvements

--- MM_Network/Set-UncHostnameAlias.ps1
+++ MM_Network/Set-UncHostnameAlias.ps1
@@ -1,14 +1,17 @@
-function Set-UncHostnameAlias($UncPath) {
+function Set-UncHostnameAlias($UncPath, $RemoteAlias) {
     if (!(Get-Module -ListAvailable PsHosts)) { throw "PsHosts module must be installed. Install-Module PsHosts" }
+    if (!($RemoteAlias)) {
+        if (!$MyInvocation.ScriptName) { throw "Either run this function within a script or provide RemoteAlias parameter" }
+        $RemoteAlias = (Split-Path -Leaf $MyInvocation.ScriptName) -replace '.ps1$'
+    }
 
     $remote_hostname = $UncPath -split '\\' | ? {$_} | select -First 1
-    $remote_alias    = (Split-Path -Leaf $MyInvocation.ScriptName) -replace '.ps1$'
 
     $hostEntry = Get-HostEntry $remote_alias* -ea 0 | ? { $_.Comment -eq $remote_hostname } | select -First 1
     if (!$hostEntry) {
         $remote_alias += (Get-HostEntry $remote_alias*).Count + 1
         Write-Verbose "Adding alias $remote_alias => $remote_hostname"
-        $remote_ip = Test-Connection -ComputerName $remote_hostname -Count 1  | % IPV4Address | % IPAddressToString
+        $remote_ip = Test-Connection -ComputerName $remote_hostname -Count 1 -ErrorAction Stop | % IPV4Address | % IPAddressToString
         Add-HostEntry -Name $remote_alias -Address $remote_ip -Force -Comment $remote_hostname | Out-Null
     } else {
         $remote_alias =  $hostEntry.Name
