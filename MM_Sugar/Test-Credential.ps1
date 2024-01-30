ee5ef42f8e2337ea6bcfb6ead8b1f7f6427f2a03
Miodrag Milić <miodrag.milic@gmail.com>
2024-01-30 10:44:04 +0100

    Test-ADCredential

--- MM_Sugar/Test-Credential.ps1
+++ MM_Sugar/Test-Credential.ps1
@@ -7,7 +7,7 @@
 #>
 function Test-Credential {
     param(
-        # A PScredential object with the username/password to test. Typically this is generated using the Get-Credential cmdlet.
+        # A PSCredential object with the username/password to test. Typically this is generated using the Get-Credential cmdlet.
         [parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [PSCredential] $Credential,
 
@@ -18,10 +18,33 @@ function Test-Credential {
 
     begin {
         Add-Type -AssemblyName System.DirectoryServices.AccountManagement
-        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::$Context) 
+        $DS = New-Object System.DirectoryServices.AccountManagement.PrincipalContext([System.DirectoryServices.AccountManagement.ContextType]::$Context)
     }
 
     process {
         $DS.ValidateCredentials( $Credential.UserName, $Credential.GetNetworkCredential().Password )
     }
+}
+
+<#
+.SYNOPSIS
+    Takes a PSCredential object and validates it against the domain
+
+.DESCRIPTION
+    Sometimes Test-Credential method doesn't work
+
+.LINK
+    https://serverfault.com/a/286003/23290
+#>
+
+function Test-ADCredential {
+    param(
+        # A PSCredential object with the username/password to test. Typically this is generated using the Get-Credential cmdlet.
+        [parameter(Mandatory=$true, ValueFromPipeline=$true)]
+        [PSCredential] $Credential
+    )
+
+    $currentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+    $domain = New-Object System.DirectoryServices.DirectoryEntry($currentDomain, $Credential.UserName, $Credential.GetNetworkCredential().Password)
+    $null -ne $domain.name
 }
